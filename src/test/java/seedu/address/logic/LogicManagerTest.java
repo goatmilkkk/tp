2cf6ad997f388741d87a537e8c668a1c3c2deb92
goatmilkkk <88575461+goatmilkkk@users.noreply.github.com>
2025-11-01 16:03:54 +0800

    Add test cases for coverage

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -68,6 +68,16 @@ public class LogicManagerTest {
     }
 
     @Test
+    public void execute_editApptOutsideAppointmentView_throwsCommandException() {
+        assertCommandException("edit-appt 1 ad/12-12-2025, 1200", MESSAGE_NOT_VIEWING_APPOINTMENT);
+    }
+
+    @Test
+    public void execute_deleteApptOutsideAppointmentView_throwsCommandException() {
+        assertCommandException("delete-appt 1", MESSAGE_NOT_VIEWING_APPOINTMENT);
+    }
+
+    @Test
     public void execute_validCommand_success() throws Exception {
         String listCommand = ListCommand.COMMAND_WORD;
         assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
